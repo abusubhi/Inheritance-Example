85baed5db9ff0f59aa4636fa0ff42c8e4b0716a2
Mohammed Aloshaibi <mohammed.aloshaiby@gmail.com>
2024-01-23 17:18:16 +0300

    first commit

---
+++ inheritance2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class PerantClass
+{
+	public PerantClass()
+	{
+		Console.WriteLine("parent class constructor called");
+	}
+
+	public PerantClass(string message)
+	{
+		Console.WriteLine($"{message}");
+	}
+
+
+}
+
+public class chiledclass : PerantClass
+{
+	/*constructor*/
+    public chiledclass() : base("Derived class controlling") 
+	{
+		Console.WriteLine("chiled class constructor called");
+	}
+
+}
+class Program 
+{
+	public static void Main(string[] args)
+	{
+		/*object to call constructor (chiledclass)*/
+		chiledclass cc= new chiledclass();
+	}
+}
